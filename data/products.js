d0c1d91d806d053f4f138a7412b30e362bc63ec5
an pham <anzxczxc@gmail.com>
2022-10-14 23:27:21 +0700

    first commit

---
+++ data/products.js
@@ -0,0 +1,800 @@
+var list_products = [{ //Sach
+    "name": "Chien thang cuoc choi cuoc song",
+    "company": "",
+    "img": "img/products/Sach/Chienthangtrochoicuocsong.jfif",
+    "price": "49.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt1"
+}, {
+    "name": "Tieng ken thien nga",
+    "company": "",
+    "img": "img/products/Sach/Tiengkenthiennga.jpg",
+    "price": "69.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt2"
+}, {
+    "name": "Nha Gia Kim",
+    "company": "",
+    "img": "img/products/Sach/Nhagiakim.jpg",
+    "price": "79.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "5.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt3"
+}, {
+    "name": "Đon Gian",
+    "company": "",
+    "img": "img/products/Sach/Dongian.jpg",
+    "price": "47.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "giamgia",
+        "value": "15.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt4"
+}, {
+    "name": "Dung De Tien Ngu Yen",
+    "company": "",
+    "img": "img/products/Sach/Dungdetiennguyen.png",
+    "price": "25.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "10.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt5"
+}, {
+    "name": "Hay Nho Ten Anh Ay",
+    "company": "",
+    "img": "img/products/Sach/Haynhotenanhay.jpg",
+    "price": "31.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "giareonline",
+        "value": "27.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt6"
+}, {
+    "name": "HOm Nay Toi That Tinh",
+    "company": "",
+    "img": "img/products/Sach/Homnaytoithattinh.jpg",
+    "price": "46.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt7"
+}, {
+    "name": "Lam Ban Voi Bau Troi",
+    "company": "",
+    "img": "img/products/Sach/Lambanvoibautroi.jpg",
+    "price": "62.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "15.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt8"
+}, {
+    "name": "Nguoi Don Tau",
+    "company": "",
+    "img": "img/products/Sach/Nguoidontau.jpg",
+    "price": "59.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Stt9"
+}, { //SachGiaoKhoa
+    "name": "Tieng Anh 10",
+    "company": "",
+    "img": "img/products/SachGiaoKhoa/Tienganh10.jpg",
+    "price": "51.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "20.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Sgk0"
+}, {
+    "name": "Toan 10",
+    "company": "",
+    "img": "img/products/SachGiaoKhoa/Toan10.jpg",
+    "price": "62.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Sgk1"
+}, {
+    "name": "Ngu Van 10",
+    "company": "",
+    "img": "img/products/SachGiaoKhoa/Nguvan10.jpg",
+    "price": "34.000",
+    "star": 4,
+    "rateCount": 16,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Sgk2"
+}, {
+    "name": "Tin Hoc 10",
+    "company": "",
+    "img": "img/products/SachGiaoKhoa/Tinhoc10.jpg",
+    "price": "85.000",
+    "star": 4,
+    "rateCount": 104,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Sgk3"
+}, {
+    "name": "Sinh Hoc 10",
+    "company": "",
+    "img": "img/products/SachGiaoKhoa/Sinhhoc10.jpg",
+    "price": "49.000",
+    "star": 4,
+    "rateCount": 80,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Sgk4"
+}, {
+    "name": "Vat Ly 10",
+    "company": "",
+    "img": "img/products/SachGiaoKhoa/Vatly10.jpg",
+    "price": "60.000",
+    "star": 5,
+    "rateCount": 87,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Sgk5"
+}, {
+    "name": "Hoa Hoc 10",
+    "company": "",
+    "img": "img/products/SachGiaoKhoa/Hoahoc10.jpg",
+    "price": "69.000",
+    "star": 4.5,
+    "rateCount": 372,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "nha giao vn",
+        "xuatsu": "Viet Nam"
+    },
+    "masp": "Sgk6"
+}, {
+    "name": "Dia Li 10",
+    "company": "Địa lí",
+    "img": "img/products/SachGiaoKhoa/Diali10.jpg",
+    "price": "77.000",
+    "star": 5,
+    "rateCount": 347,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Sgk7"
+}, {
+    "name": "Cong Nghe 10",
+    "company": "",
+    "img": "img/products/SachGiaoKhoa/Congnghe10.jpg",
+    "price": "71.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Sgk8"
+}, { //SachHoatHinh
+    "name": "5 Centimet Tren Giay",
+    "company": "",
+    "img": "img/products/SachHoatHinh/5centimettrengiay.jpg",
+    "price": "98.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San0"
+}, {
+    "name": "Co Gai Vuot Thoi Gian",
+    "company": "",
+    "img": "img/products/SachHoatHinh/Cogaivuotthoigian.jpg",
+    "price": "54.000",
+    "star": 5,
+    "rateCount": 54,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San1"
+}, {
+    "name": "Dieu Bi Mat",
+    "company": "",
+    "img": "img/products/SachHoatHinh/Dieubimat.jpg",
+    "price": "99.000",
+    "star": 5,
+    "rateCount": 999,
+    "promo": {
+        "name": "giamgia",
+        "value": "15.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San2"
+}, {
+    "name": "Ngoi Nha Nghin Hang Lnag",
+    "company": "",
+    "img": "img/products/SachHoatHinh/Ngoinhanghinhanhlang.jpg",
+    "price": "89.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San3"
+}, {
+    "name": "Ty Quay",
+    "company": "Realme",
+    "img": "img/products/SachHoatHinh/Tyquay.jpg",
+    "price": "69.000",
+    "star": 5,
+    "rateCount": 16,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San4"
+}, {
+    "name": "Chuyen Con E Co Chong",
+    "company": "Realme",
+    "img": "img/products/SachHoatHinh/Chuyenconecochong.jpg",
+    "price": "49.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San5"
+}, {
+    "name": "One Piece 2",
+    "company": "Realme",
+    "img": "img/products/SachHoatHinh/Onepiece2.jpg",
+    "price": "80.000",
+    "star": 4,
+    "rateCount": 4,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San6"
+}, {
+    "name": "One Piece 57",
+    "company": "Realme",
+    "img": "img/products/SachHoatHinh/Onepiece57.jpg",
+    "price": "80.000",
+    "star": 4,
+    "rateCount": 11,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "Echiro Oda",
+        "xuatsu": "Nhật bản"
+    },
+    "masp": "San7"
+}, {
+    "name": "Shin Cau Be But Chi 47",
+    "company": "Philips",
+    "img": "img/products/SachHoatHinh/Shincaubebutchi47.jpg",
+    "price": "75.000",
+    "star": 5,
+    "rateCount": 6,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San8"
+}, {
+    "name": "NARUTO 1",
+    "company": "Philips",
+    "img": "img/products/SachHoatHinh/Naruto1.jpg",
+    "price": "63.000",
+    "star": 3,
+    "rateCount": 30,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San9"
+}, {
+    "name": "Conan 13",
+    "company": "Philips",
+    "img": "img/products/SachHoatHinh/Conan13.jpg",
+    "price": "58.000",
+    "star": 5,
+    "rateCount": 126,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San10"
+}, {
+    "name": "Doremon 1",
+    "company": "",
+    "img": "img/products/SachHoatHinh/Doremon1.jpg",
+    "price": "45.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San11"
+}, {
+    "name": "Tua And The Elephant",
+    "company": "",
+    "img": "img/products/SachHoatHinh/Elephant.jpg",
+    "price": "74.000",
+    "star": 5,
+    "rateCount": 168,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+    "detail": {
+        
+    },
+    "masp": "San12"
+}, {
+    "name": "Muon Nhanh THi Phai Tu Tu",
+    "company": "",
+    "img": "img/products/SachHoatHinh/Muonnhanhthiphaitutu.jpg",
+    "price": "49.000",
+    "star": 4,
+    "rateCount": 60,
+    "promo": {
+        "name": "giamgia",
+        "value": "5.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "San13"
+}, { //SachVanHoc
+    "name": "Victor Hugo - Nhung Nguoi Khon Kho 1",
+    "company": "",
+    "img": "img/products/SachVanHoc/Nhungnguoikhonkho1.jpg",
+    "price": "220.000",
+    "star": 4,
+    "rateCount": 60,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Svh0"
+}, {
+    "name": "Victor Huygo - Notre-Dame de Paris ",
+    "company": "Mobell",
+    "img": "img/products/SachVanHoc/Thanggunhathoducba.jpg",
+    "price": "250.000",
+    "star": 4,
+    "rateCount": 37,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Svh1"
+}, {
+    "name": "Tieng Chim Hot Trong Bui Man Gai",
+    "company": "Mobell",
+    "img": "img/products/SachVanHoc/Tiengchimhottrongbuimangai.jpg",
+    "price": "590.000",
+    "star": 4,
+    "rateCount": 58,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Svh2"
+}, {
+    "name": "Nha Tho Paris",
+    "company": "Mobell",
+    "img": "img/products/SachVanHoc/Nhathoducbaparis.jpg",
+    "price": "290.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Svh3"
+}, {
+    "name": "Ba Ga Cung Thuyen",
+    "company": "",
+    "img": "img/products/SachVanHoc/Bagacungthuyen.jpg",
+    "price": "890.000",
+    "star": 4,
+    "rateCount": 12,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Svh4"
+}, {
+    "name": "45 Do F",
+    "company": "",
+    "img": "img/products/SachVanHoc/45dof.jpg",
+    "price": "350.000",
+    "star": 5,
+    "rateCount": 3,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Svh5"
+}, {
+    "name": "The Secret Tree",
+    "company": "",
+    "img": "img/products/SachVanHoc/Thesecrettree.jpg",
+    "price": "160.000",
+    "star": 5,
+    "rateCount": 302,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Svh6"
+}, {
+    "name": "Ngay Cuoi CUng Cua Mot Tu Tu",
+    "company": "",
+    "img": "img/products/SachVanHoc/Ngaycuoicungcuamottutu.jfif",
+    "price": "170.000",
+    "star": 5,
+    "rateCount": 33,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "Svh7"
+}, { //SachTieuThuyet
+    "name": "33 Ngay That Tinh",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/33ngaythattinh.jpg",
+    "price": "390.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT0"
+}, {
+    "name": "Cau Yeu Toi Phai Khong",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/Cauyeutoiphaikhong.jpg",
+    "price": "590.000",
+    "star": 3,
+    "rateCount": 3,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT1"
+}, {
+    "name": "Du Gan Ma Khong Dau Don - Du Xa Ma Khong Co Don",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/Duganduxa.jfif",
+    "price": "280.000",
+    "star": 4,
+    "rateCount": 2,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT2"
+}, {
+    "name": "Hoang Tu Be",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/Hoangtube.jpg",
+    "price": "690.000",
+    "star": 4,
+    "rateCount": 12,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT3"
+}, {
+    "name": "So Do",
+    "company": "Motorola",
+    "img": "img/products/SachTieuThuyet/Sodo.jpg",
+    "price": "290.000",
+    "star": 4,
+    "rateCount": 14,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT4"
+}, {
+    "name": "Song La Am Thanh Bung No",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/Songlaamthambungno.jfif",
+    "price": "990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "500.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT5"
+}, {
+    "name": "Thach Lam",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/Thachlam.jpg",
+    "price": "190.000",
+    "star": 5,
+    "rateCount": 230,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT6"
+}, {
+    "name": "Thoi Gian Tuoi Dep",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/Thoigiantuoidep.jpg",
+    "price": "490.000",
+    "star": 5,
+    "rateCount": 16,
+    "promo": {
+        "name": "giamgia",
+        "value": "30.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT7"
+}, {
+    "name": "Toi Bi Bo Bat Coc",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/Toibibobatcoc.jpg",
+    "price": "300.000",
+    "star": 4,
+    "rateCount": 5,
+    "promo": {
+        "name": "giareonline",
+        "value": "150.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT8"
+}, {
+    "name": "Tokyo On Foot",
+    "company": "",
+    "img": "img/products/SachTieuThuyet/Tokyoonfoot.jpg",
+    "price": "299.000",
+    "star": 4,
+    "rateCount": 804,
+    "promo": {
+        "name": "giareonline",
+        "value": "199.000"
+    },
+    "detail": {
+        "tacgia": "ABC",
+        "xuatsu": "Tokyo"
+    },
+    "masp": "STT9"
+}]
